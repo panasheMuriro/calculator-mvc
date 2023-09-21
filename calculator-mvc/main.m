15d7c31702262f90cada20c3e322cd503cca8870
murirop <panashemuriro2023@u.northwestern.edu>
2023-09-20 20:27:58 -0500

    Initial Commit

---
+++ calculator-mvc/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  calculator-mvc
+//
+//  Created by Panashe on 9/20/23.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
